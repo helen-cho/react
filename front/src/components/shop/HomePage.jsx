8b0db90ef942b36a1972ec61646840c91fbd4594
helen-cho <hd9536@daum.net>
2023-10-31 17:51:33 +0900

    ver16

--- front/src/components/shop/HomePage.jsx
+++ front/src/components/shop/HomePage.jsx
@@ -3,27 +3,45 @@ import React, { useEffect, useState } from 'react'
 import {Row, Col, Spinner, Card} from 'react-bootstrap'
 import {BsHeartFill, BsHeart} from 'react-icons/bs'
 import {BiMessageDetail} from 'react-icons/bi'
+import Pagination from "react-js-pagination";
+import './Pagination.css';
+import {useNavigate, useLocation} from 'react-router-dom'
 
 const HomePage = () => {
     const [books, setBooks] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [total, setTotal] = useState(0);
+    const navi = useNavigate();
+
+    const location=useLocation();
+    const search=new URLSearchParams(location.search);
+    const page = search.get("page") ? parseInt(search.get("page")) : 1;
+    const path = location.pathname;
 
     const getBooks = async () => {
-        const url = `/books/list.json?query=&page=1&size=6&uid=${sessionStorage.getItem("uid")}`;
+        const url = `/books/list.json?query=&page=${page}&size=6&uid=${sessionStorage.getItem("uid")}`;
         setLoading(true);
         const res = await axios(url);
         //console.log(res.data);
         setBooks(res.data.list);
+        setTotal(res.data.total);
         setLoading(false);
     }
 
     useEffect(() => {
         getBooks();
-    }, []);
+    }, [location]);
+
+    const onChangePage = (page) => {
+        navi(`${path}?query=&page=${page}&size=6&uid=${sessionStorage.getItem("uid")}`);
+    }
 
     if(loading) return <div className='my-5 text-center'><Spinner variant='primary'/></div>
     return (
         <div className='my-5'>
+            <Row>
+                <Col>검색수: {total}권</Col>
+            </Row>
             <Row>
                 {books.map(book=>
                     <Col xs={6} md={4} lg={2} className='mb-3'>
@@ -33,21 +51,29 @@ const HomePage = () => {
                                 <small className='ellipsis mt-2'>{book.title}</small>
                             </Card.Body>
                             <Card.Footer className="text-end">
-                                <span>
-                                    <span className='heart'>{book.ucnt === 0 ? <BsHeart/>:<BsHeartFill/>}</span>
-                                    <small className='ms-1'>{book.fcnt}</small>
-                                </span>
                                 {book.rcnt===0 ||
-                                    <span className='ms-3'>
-                                        <BiMessageDetail/>
-                                        <small className='ms-1'>{book.rcnt}</small>
+                                    <span>
+                                        <span className='message'><BiMessageDetail/></span>
+                                        <span className='ms-1 rcnt'>{book.rcnt}</span>
                                     </span>
                                 }
+                                <span className='ms-3'>
+                                    <span className='heart'>{book.ucnt === 0 ? <BsHeart/>:<BsHeartFill/>}</span>
+                                    <span className='ms-1 fcnt'>{book.fcnt}</span>
+                                </span>
                             </Card.Footer>
                         </Card>
                     </Col>
                 )}
             </Row>
+            <Pagination
+                activePage={page}
+                itemsCountPerPage={6}
+                totalItemsCount={total}
+                pageRangeDisplayed={10}
+                prevPageText={"‹"}
+                nextPageText={"›"}
+                onChange={onChangePage}/>
         </div>
     )
 }
