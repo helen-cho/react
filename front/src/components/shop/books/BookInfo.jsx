3c66d15b23dfc5ae644daff1cd1b3412847d6e86
helen-cho <hd9536@daum.net>
2023-11-01 11:46:33 +0900

    ver17

--- front/src/components/shop/books/BookInfo.jsx
+++ front/src/components/shop/books/BookInfo.jsx
@@ -1,19 +1,23 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { NavLink, useParams } from 'react-router-dom'
+import { useNavigate, useLocation, useParams } from 'react-router-dom'
 import { Spinner, Row, Col, Card, Button } from 'react-bootstrap';
 import {BsHeartFill, BsHeart} from 'react-icons/bs'
 import {BiMessageDetail} from 'react-icons/bi'
 
 const BookInfo = () => {
+    const navi = useNavigate();
+    const location = useLocation();
+    //console.log('...........', location.pathname);
+
     const {bid} = useParams();
     const [book, setBook] = useState('');
     const [loading, setLoading] = useState(false);
 
     const getBook = async() => {
         setLoading(true);
-        const res =await axios(`/books/read/${bid}`);
-        console.log(res.data);
+        const res =await axios(`/books/read/${bid}?uid=${sessionStorage.getItem("uid")}`);
+        //console.log(res.data);
         setBook(res.data);
         setLoading(false);
     }
@@ -21,6 +25,23 @@ const BookInfo = () => {
     useEffect(()=>{
         getBook();
     }, []);
+
+    const onClickHeart = async(bid) => {
+        if(sessionStorage.getItem("uid")) {
+            await axios.post('/books/insert/favorite',
+                {uid:sessionStorage.getItem("uid"), bid: bid});
+            getBook();
+        }else{
+            sessionStorage.setItem("target", location.pathname);
+            navi('/users/login');
+        }
+    }
+
+    const onClickFillHeart = async(bid) => {
+        await axios.post('/books/delete/favorite',
+                {uid:sessionStorage.getItem("uid"), bid: bid});
+            getBook();
+    }
 
     if(loading) return <div className='my-5 text-center'><Spinner variant='primary'/></div>
     return (
@@ -32,13 +53,13 @@ const BookInfo = () => {
                         <img src={book.image} width="100%"/>
                     </Col>
                     <Col className='ms-3'>
-                        <h5>{book.title}</h5>
+                        <h5 className='ellipsis'>{book.title}</h5>
                         <hr/>
                         <div className='mb-2'>가격: {book.fmtprice}원</div>
-                        <div className='ellipsis mb-2'>저자: {book.authors}</div>
-                        <div className='ellipsis mb-2'>출판사: {book.publisher}</div>
-                        <div className='ellipsis mb-2'>등록일: {book.fmtdate}</div>
-                        <div className='ellipsis'>ISBN: {book.isbn}</div>
+                        <div className='ellipsis'>저자: {book.authors}</div>
+                        <div className='ellipsis'>출판사: {book.publisher}</div>
+                        <div className='ellipsis'>등록일: {book.fmtdate}</div>
+                        <div className='ellipsis mb-2'>ISBN: {book.isbn}</div>
                         {book.rcnt===0 ||
                             <span>
                                 <span className='message'><BiMessageDetail/></span>
@@ -47,9 +68,9 @@ const BookInfo = () => {
                         }
                         <span className='ms-3'>
                             <span className='heart'>{book.ucnt === 0 ? 
-                                <BsHeart/>
+                                <BsHeart onClick={()=>onClickHeart(book.bid)}/>
                                 :
-                                <BsHeartFill/>}
+                                <BsHeartFill onClick={()=>onClickFillHeart(book.bid)}/>}
                             </span>
                             <span className='ms-1 fcnt'>{book.fcnt}</span>
                         </span>
