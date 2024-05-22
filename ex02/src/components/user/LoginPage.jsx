e5b84cba7753addff75ad6da5edc444a7179eba3
helen-cho <hd9536@daum.net>
2024-05-22 18:01:00 +0900

    로그인

--- ex02/src/components/user/LoginPage.jsx
+++ ex02/src/components/user/LoginPage.jsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Row, Col, Card, Form, InputGroup, Button} from 'react-bootstrap'
 const LoginPage = () => {
+  
+  const [form, setForm] = useState({
+    email:'red@test.com',
+    pass:'12341234'
+  });
+  const {email, pass} = form;
+  const onChangeForm = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]:e.target.value
+    });
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if(email==='' || pass==='') {
+      alert('이메일과 패스워드를 입력하세요!');
+      return;
+    }
+    //로그인체크
+    alert(`${email}\n${pass}`);
+  }
+
   return (
     <div className='my-5 userLogin'>
       <Row className='justify-content-center' >
@@ -8,16 +31,18 @@ const LoginPage = () => {
           <Card>
             <Card.Header><h3 className='text-center'>로그인</h3></Card.Header>
             <Card.Body>
-              <form>
+              <form onSubmit={onSubmit}>
                 <InputGroup className='mb-2' >
                   <InputGroup.Text className='title justify-content-center'>아이디</InputGroup.Text>
-                  <Form.Control placeholder='아이디'/>
+                  <Form.Control name="email" value={email} onChange={onChangeForm}
+                    placeholder='이메일'/>
                 </InputGroup>
-                <InputGroup className='mb-2'>
+                <InputGroup className='mb-3'>
                   <InputGroup.Text className='title justify-content-center'>비밀번호</InputGroup.Text>
-                  <Form.Control type="password" placeholder='비밀번호'/>
+                  <Form.Control name="pass" value={pass} onChange={onChangeForm}
+                    type="password" placeholder='비밀번호'/>
                 </InputGroup>
-                <Button className='w-100'>로그인</Button>
+                <Button className='w-100' type='submit'>로그인</Button>
               </form>
             </Card.Body>
           </Card>
