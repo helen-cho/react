caabd4be2daf808b3961e2f26eb11b9e4b450e70
helen-cho <hd9536@daum.net>
2024-05-22 11:09:16 +0900

    라우터

--- ex02/src/Header.jsx
+++ ex02/src/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
     return (
         <div>
-            <img src="http://via.placeholder.com/960x150" width="100%"/>
+            <img src="/images/header02.png" width="100%"/>
         </div>
     )
 }
---
+++ ex02/src/components/book/BookSearch.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BookSearch = () => {
+    return (
+        <div className='my-5'>
+            <h1 className='text-center'>도서검색</h1>
+        </div>
+    )
+}
+
+export default BookSearch
