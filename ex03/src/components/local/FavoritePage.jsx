abaaa8296c9aeb2bb510a7941d4de063838699e3
helen-cho <hd9536@daum.net>
2024-05-23 17:16:26 +0900

    즐겨찾기

--- ex03/src/components/local/FavoritePage.jsx
+++ ex03/src/components/local/FavoritePage.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { app } from '../../firebaseInit'
+import { getDatabase, ref, onValue } from 'firebase/database'
 
 const FavoritePage = () => {
+  const db = getDatabase(app);
+  const uid= sessionStorage.getItem('uid');
+
+  const callAPI = () => {
+    onValue(ref(db, `favorite/${uid}`), res=>{
+      let rows= [];
+      let count=0;
+      res.forEach(row=>{
+        count++;
+        rows.push({no:count, key:row.key, ...row.val()});
+      });
+      console.log(rows);
+    });
+  }
+
+  useEffect(()=>{
+    callAPI();
+  }, []);
+
   return (
     <div className='my-5'>
       <h1 className='my-5 text-center'>즐겨찾기</h1>
