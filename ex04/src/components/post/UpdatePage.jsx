19a5c8d9fd81d4d346b5bfdfccccb3285df6e270
helen-cho <hd9536@daum.net>
2024-05-27 11:01:46 +0900

    게시글수정

--- ex04/src/components/post/UpdatePage.jsx
+++ ex04/src/components/post/UpdatePage.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import {Row, Col, Button, Form} from 'react-bootstrap'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { app } from '../../firebaseInit'
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
 
 const UpdatePage = () => {
+  const navi = useNavigate();
   const [loading, setLoading] = useState(false);
   const db = getFirestore(app);
   const {id} = useParams();
@@ -23,16 +24,32 @@ const UpdatePage = () => {
     callAPI();
   }, []);
 
+  const onChangeForm = (e) => {
+    setForm({...form, [e.target.name]:e.target.value});
+  }
+
+  const onSubmit = async(e) => {
+    e.preventDefault();
+    if(!window.confirm(`${id}번 게시글을 수정하실래요?`)) return;
+    //게시글수정
+    setLoading(true);
+    await setDoc(doc(db,`posts/${id}`), form);
+    setLoading(false);
+    navi(`/post/read/${id}`);
+  }
+
   if(loading) return <h1 className='my-5 text-center'>로딩중......</h1>
   return (
     <Row className='justify-content-center my-5'>
       <Col xs={12} md={10} lg={8}>
         <h1 className='text-center mb-5'>게시글수정</h1>
-        <form>
-          <Form.Control className='mb-2' value={title}/>
-          <Form.Control as="textarea" rows={15} className='mb-3' value={body}/>
+        <form onSubmit={onSubmit}>
+          <Form.Control name="title" onChange={onChangeForm}
+            className='mb-2' value={title}/>
+          <Form.Control name="body" onChange={onChangeForm}
+            as="textarea" rows={15} className='mb-3' value={body}/>
           <div className='text-center'>
-            <Button className='px-5'>수정</Button>
+            <Button className='px-5' type="submit">수정</Button>
             <Button className='px-5' variant='secondary ms-2'>취소</Button>
           </div>  
         </form>
