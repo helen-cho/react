c0bc26ef19e8ba2516fd7738f3d4056498c171d8
helen-cho <hd9536@daum.net>
2024-05-30 13:19:17 +0900

    이미지변경

--- shop/src/components/books/ModalImage.jsx
+++ shop/src/components/books/ModalImage.jsx
@@ -1,8 +1,13 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
 const ModalImage = ({bigimage}) => {
+  const refImage = useRef(null);
+  const style ={
+    cursor:'pointer'
+  }
+
   const [bigImage, setBigImage] = useState({
     fileName:'',
     file:null
@@ -16,14 +21,35 @@ const ModalImage = ({bigimage}) => {
   }
 
   const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+
+  const handleClose = () =>{
+    setShow(false);
+    setBigImage({
+      file:null,
+      fileName:bigimage
+    })
+  } 
+  const handleShow = () => {
+    setShow(true);
+    setBigImage({
+      file:null,
+      fileName:bigimage
+    })
+  }
+
+  const onClickSave = () => {
+    if(file==null) {
+      alert("저장할 이미지를 선택하세요!");
+      return;
+    }
+    if(!window.confirm('이미지를 저장하실래요?')) return;
+    //사진저장(업로드);
+  }
 
   return (
     <>
-      <img onClick={handleShow}
+      <img onClick={handleShow} style={style}
         src={bigimage || "http://via.placeholder.com/120x170"} width="100%"/>
-
       <Modal style={{top:'10%'}}
         show={show}
         onHide={handleClose}
@@ -33,14 +59,15 @@ const ModalImage = ({bigimage}) => {
           <Modal.Title>이미지변경</Modal.Title>
         </Modal.Header>
         <Modal.Body className='text-center'>
-          <img src={fileName || "http://via.placeholder.com/120x170"} width="80%"/>
-          <input type="file" onChange={onChangeFile}/>
+          <img style={style} onClick={()=>refImage.current.click()}
+            src={fileName || "http://via.placeholder.com/120x170"} width="80%"/>
+          <input ref={refImage} type="file" onChange={onChangeFile} style={{display:'none'}}/>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             닫기
           </Button>
-          <Button variant="primary">저장</Button>
+          <Button onClick={onClickSave} variant="primary">저장</Button>
         </Modal.Footer>
       </Modal>
     </>
