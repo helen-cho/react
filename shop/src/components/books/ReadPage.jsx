4d036373ec5518a773d0341cbfe3334214a84c8f
helen-cho <hd9536@daum.net>
2024-05-31 11:46:25 +0900

    도서정보

--- shop/src/components/books/ReadPage.jsx
+++ shop/src/components/books/ReadPage.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import {Row, Col, Card, Button } from 'react-bootstrap'
 import { useParams } from 'react-router-dom'
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 
 const ReadPage = () => {
   const {bid} = useParams();
@@ -15,9 +16,12 @@ const ReadPage = () => {
     isbn:'',
     fmtdate:'',
     fmtprice:'',
-    publisher:''
+    publisher:'',
+    lcnt:'',
+    ucnt:''
   });
-  const {author, title, bigimage, contents, isbn, fmtdate, fmtprice, publisher} = book;
+  const {author, title, bigimage, contents, isbn, fmtdate, 
+    lcnt, ucnt, fmtprice, publisher} = book;
   
   const callAPI = async() => {
     const res=await axios.get(`/books/read/${bid}?uid=${uid}`);
@@ -35,18 +39,27 @@ const ReadPage = () => {
         <Card>
           <Card.Body>
             <Row>
-              <Col md={6} className='text-center'>
+              <Col md={6} className='text-center mt-3'>
                 <img src={bigimage || "http://via.placeholder.com/120x170"} width="80%"/>
               </Col>
               <Col className='my-3'>
-                <div><h5>[{bid}] {title}</h5></div>
+                <div>
+                  <span className='me-2'>[{bid}] {title}</span>
+                  {ucnt === 0 ?
+                    <FaRegHeart className='heart'/>
+                    :
+                    <FaHeart className='heart'/>
+                  }
+                  <span style={{fontSize:'12px'}}>{lcnt}</span>
+                </div>
                 <hr/>
-                <div>저자: {author}</div>
-                <div>출판사: {publisher}</div>
-                <div>ISBN: {isbn}</div>
-                <div>가격: {fmtprice}원</div>
-                <div>수정일: {fmtdate}</div>
-                <div className='text-center mt-3'>
+                <div className='mb-2'>저자: {author}</div>
+                <div className='mb-2'>출판사: {publisher}</div>
+                <div className='mb-2'>ISBN: {isbn}</div>
+                <div className='mb-2'>가격: {fmtprice}원</div>
+                <div className='mb-2'>수정일: {fmtdate}</div>
+                <hr/>
+                <div className='mt-3'>
                   <Button className='px-3 me-2' variant='warning'>바로구매</Button>
                   <Button className='px-3' variant='success'>장바구니</Button>
                 </div>
