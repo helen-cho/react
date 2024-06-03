e73ed66c0dd5eb190ece468e3aeb76caeae8a419
helen-cho <hd9536@daum.net>
2024-06-03 11:57:53 +0900

    장바구니

--- shop/src/components/orders/CartPage.jsx
+++ shop/src/components/orders/CartPage.jsx
@@ -1,13 +1,21 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { Row, Col, Table, Button } from 'react-bootstrap';
+import { Row, Col, Table, Button, Alert } from 'react-bootstrap';
 const CartPage = () => {
+  const [total, setTotal] = useState(0);
   const [books, setBooks] = useState([]);
   const uid=sessionStorage.getItem('uid');
 
   const callAPI = async() => {
     const res=await axios.get(`/cart/list?uid=${uid}`);
-    setBooks(res.data);
+    const data=res.data.map(book=>book && {...book, sum:book.qnt*book.price});
+    setBooks(data);
+
+    let totalSum=0;
+    data.forEach(book=>{
+      totalSum+=book.sum;
+    });
+    setTotal(totalSum);
   }
 
   useEffect(()=>{
@@ -20,11 +28,12 @@ const CartPage = () => {
         <h1 className='text-center mb-3'>장바구니</h1>
         <Table striped bordered hover>
           <thead>
-            <tr>
+            <tr className='text-center'>
               <td>ID.</td>
               <td>도서명</td>
               <td>가격</td>
               <td>수량</td>
+              <td>금액</td>
             </tr>  
           </thead>
           <tbody>
@@ -37,10 +46,12 @@ const CartPage = () => {
                 </td>
                 <td>{book.fmtprice}원</td>
                 <td>{book.qnt}</td>
+                <td>{book.sum.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원</td>
               </tr>
             )}
           </tbody>
         </Table>
+        <Alert className='text-end'>총합계: {total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원</Alert>
       </Col>
     </Row>
   )
