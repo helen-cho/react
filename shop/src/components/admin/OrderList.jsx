976f0e35cfc6e5beca1f09a9f70445dd260f920d
helen-cho <hd9536@daum.net>
2024-06-04 15:49:07 +0900

    주문

--- shop/src/components/admin/OrderList.jsx
+++ shop/src/components/admin/OrderList.jsx
@@ -19,6 +19,19 @@ const OrderList = () => {
     callAPI();
   }, []);
 
+  const onChangeStatus = (e, pid)=> {
+    const data=orders.map(order=>order.pid===pid ? {...order, status:e.target.value}:order);
+    setOrders(data);
+  }
+
+  const onUpdateStatus = async(pid, status)=> {
+    if(!window.confirm(`${pid}번 주문의 상태를 ${status}로 변경하실래요?`)) return;
+    const res=await axios.post('/orders/status', {pid, status});
+    if(res.data.result===1){
+      alert("상태변경완료!");
+    }
+  }
+
   return (
     <div className='my-5'>
       <h1 className='text-center mb-5'>주문관리</h1>
@@ -45,13 +58,14 @@ const OrderList = () => {
               <td>{order.fmtsum}원</td>
               <td>
                 <InputGroup>
-                  <Form.Select value={order.status}>
-                    <option>결제대기</option>
-                    <option>결제확인</option>
-                    <option>배송준비</option>
-                    <option>배송완료</option>
+                  <Form.Select onChange={(e)=>onChangeStatus(e, order.pid)} value={order.status}>
+                    <option value="0">결제대기</option>
+                    <option value="1">결제확인</option>
+                    <option value="2">배송준비</option>
+                    <option value="3">배송완료</option>
+                    <option value="4">주문완료</option>
                   </Form.Select>
-                  <Button>변경</Button>
+                  <Button onClick={()=>onUpdateStatus(order.pid, order.status)}>변경</Button>
                 </InputGroup>
               </td>
             </tr>
