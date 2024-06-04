6e24892b5224e993615b1d86b7a2006d938f937c
helen-cho <hd9536@daum.net>
2024-06-04 17:18:37 +0900

    주문목록

--- shop/src/components/admin/OrderList.jsx
+++ shop/src/components/admin/OrderList.jsx
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { Table,Form, Button, InputGroup, Row, Col } from 'react-bootstrap';
+import '../Paging.css'
+import  Pagination from 'react-js-pagination'
 
 const OrderList = () => {
   const [count, setCount] = useState(0);
   const [orders, setOrders] = useState([]);
-  const [key, setKey] = useState('uname');
+  const [key, setKey] = useState('status');
   const [word, setWord] = useState('');
   const [page, setPage] = useState(1);
   const [size, setSize] = useState(5);
@@ -20,7 +22,7 @@ const OrderList = () => {
 
   useEffect(()=>{
     callAPI();
-  }, []);
+  }, [page]);
 
   const onChangeStatus = (e, pid)=> {
     const data=orders.map(order=>order.pid===pid ? {...order, status:e.target.value}:order);
@@ -36,14 +38,17 @@ const OrderList = () => {
   }
   const onSubmit = (e) => {
     e.preventDefault();
+    setPage(1);
     callAPI();
   }
 
-  const onSubmitStatus = async(e) => {
-    e.preventDefault();
-    const res=await axios.get(`/orders/admin/list?key=status&word=${status}&page=${page}&size=${size}`);
-    setOrders(res.data.documents);
-    setCount(res.data.count);
+  const onChangeKey = (e) => {
+    setKey(e.target.value);
+    if(e.target.value==='status'){
+      setWord(0)
+    }else{
+      setWord('');
+    }
   }
 
   return (
@@ -53,33 +58,30 @@ const OrderList = () => {
         <Col xs={8} md={6} lg={5}>
           <form onSubmit={onSubmit}>
             <InputGroup>
-              <Form.Select className='me-2' value={key} onChange={(e)=>setKey(e.target.value)}>
+              <Form.Select className='me-2' value={key} onChange={onChangeKey}>
+                <option value="status">주문상태</option>
                 <option value="uid">아이디</option>
                 <option value="uname">주문자명</option>
                 <option value="phone">전화</option>
                 <option value="address1">배송지</option>
               </Form.Select>
-              <Form.Control value={word} onChange={(e)=>setWord(e.target.value)} placeholder='검색어'/>
+              {key==='status' ? 
+                <Form.Select value={word} onChange={(e)=>setWord(e.target.value)}>
+                  <option value="0" selected>결제대기</option>
+                  <option value="1">결제확인</option>
+                  <option value="2">배송준비</option>
+                  <option value="3">배송완료</option>
+                  <option value="4">주문완료</option>
+                </Form.Select>
+                :
+                <Form.Control value={word} onChange={(e)=>setWord(e.target.value)} placeholder='검색어'/>
+              }
               <Button type="submit">검색</Button>
             </InputGroup>
           </form>
         </Col>
         <Col className='pt-2'>
           <span>검색수: {count}건</span>
-        </Col>
-        <Col>
-          <form onSubmit={onSubmitStatus} style={{width:'180px', float:'right'}}>
-            <InputGroup>
-              <Form.Select value={status} onChange={(e)=>setStatus(e.target.value)}>
-                <option value="0">결제대기</option>
-                <option value="1">결제확인</option>
-                <option value="2">배송준비</option>
-                <option value="3">배송완료</option>
-                <option value="4">주문완료</option>
-              </Form.Select>
-              <Button type="submit">검색</Button>
-            </InputGroup>
-          </form>
         </Col>
       </Row>
       <Table striped bordered hover>
@@ -119,6 +121,16 @@ const OrderList = () => {
           )}
         </tbody>
       </Table>
+      {count > size && 
+        <Pagination
+          activePage={page}
+          itemsCountPerPage={size}
+          totalItemsCount={count}
+          pageRangeDisplayed={5}
+          prevPageText={"‹"}
+          nextPageText={"›"}
+          onChange={ (e)=>setPage(e) }/>
+      }
     </div>
   )
 }
