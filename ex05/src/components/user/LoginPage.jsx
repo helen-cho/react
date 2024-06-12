f8d98d43961be890afdd582b28dad0d9fdb5b652
helen-cho <hd9536@daum.net>
2024-06-12 13:15:43 +0900

    로그인

--- ex05/src/components/user/LoginPage.jsx
+++ ex05/src/components/user/LoginPage.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 import {Row, Col, Card, InputGroup, Form, Button} from 'react-bootstrap'
 const LoginPage = () => {
+  const [form, setForm] = useState({
+    uid:'red',
+    upass:'pass'
+  });
+  const {uid, upass} = form;
+  const onChangeForm = (e) => {
+    setForm({...form, [e.target.name]:e.target.value});
+  }
+
+  const onSubmit = async(e) => {
+    e.preventDefault();
+    const res=await axios.get(`/users/${uid}`);
+    console.log(res.data);
+    
+    if(!res.data){
+      alert("아이디가 존재하지 않습니다!");
+    }else if(upass === res.data.upass){
+      alert("성공!");
+    }else{
+      alert("비밀번호가 일치하지 않습니다!");
+    }
+  }
+
   return (
     <Row className='justify-content-center my-5'>
       <Col xs={10} md={6} lg={4}>
@@ -9,14 +33,14 @@ const LoginPage = () => {
             <h3 className='text-center'>로그인</h3>
           </Card.Header>
           <Card.Body>
-            <form>
+            <form onSubmit={onSubmit}>
               <InputGroup className='mb-2'>
                 <InputGroup.Text className='title'>아이디</InputGroup.Text>
-                <Form.Control/>
+                <Form.Control name="uid" value={uid} onChange={onChangeForm}/>
               </InputGroup>
               <InputGroup className='mb-4'>
                 <InputGroup.Text className='title'>비밀번호</InputGroup.Text>
-                <Form.Control/>
+                <Form.Control name="upass" value={upass} type="password" onChange={onChangeForm}/>
               </InputGroup>
               <Button type="submit" className='w-100'>로그인</Button>
             </form>
