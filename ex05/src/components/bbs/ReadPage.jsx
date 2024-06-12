a7e41bd82625088c45e2ad9f19cfb3c92ee0c84a
helen-cho <hd9536@daum.net>
2024-06-12 12:42:21 +0900

    게시글정보

--- ex05/src/components/bbs/ReadPage.jsx
+++ ex05/src/components/bbs/ReadPage.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { Row, Col, Card} from 'react-bootstrap'
 
 const ReadPage = () => {
+  const [form, setForm] = useState('');
+  const {bid} = useParams();
+
+  const callAPI = async() => {
+    const res=await axios.get(`/bbs/${bid}`);
+    console.log(res.data);
+    setForm(res.data);
+  }
+
+  useEffect(()=>{
+    callAPI();
+  }, []);
+
   return (
     <div className='my-5'>
-      <h1 className='text-center'>게시글정보</h1>
+      <h1 className='text-center mb-5'>{bid} 게시글정보</h1>
+      <Row className='justify-content-center'>
+        <Col xs={12} md={10} lg={8}>
+          <Card>
+            <Card.Header>
+              <h5>{form.title}</h5>
+            </Card.Header>
+            <Card.Body style={{whiteSpace:'pre-wrap'}}>
+              {form.contents}
+            </Card.Body>
+            <Card.Footer className='text-muted'>
+              Created by {form.uname} on {form.regDate}
+            </Card.Footer>
+          </Card>
+        </Col>
+      </Row>
     </div>
   )
 }
