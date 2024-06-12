3bc571483ea1f137aa6dadeccd6216f1c94c9a0f
helen-cho <hd9536@daum.net>
2024-06-12 17:18:49 +0900

    글수정

--- ex05/src/components/bbs/UpdatePage.jsx
+++ ex05/src/components/bbs/UpdatePage.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import {Row, Col, Form, Button} from 'react-bootstrap'
+import { useParams } from 'react-router-dom'
 
 const UpdatePage = () => {
+  const {bid} = useParams();
+  const [form, setForm] = useState({
+    bid: bid,
+    title:'',
+    contents:''
+  });
+  const {title, contents} = form;
+
+  const callAPI = async() => {
+    const res=await axios.get(`/bbs/${bid}`);
+    setForm(res.data);
+  }
+
+  useEffect(()=>{
+    callAPI();
+  }, []);
+
   return (
     <div className='my-5'>
       <h1 className='text-center mb-5'>게시글수정</h1>
       <Row className='justify-content-center'>
         <Col xs={12} md={10} lg={8}>
           <form>
-            <Form.Control className='mb-2'/>
-            <Form.Control as="textarea" rows={10}/>
+            <Form.Control value={title} name="title"
+              className='mb-2'/>
+            <Form.Control value={contents} name="contents"
+              as="textarea" rows={10}/>
             <div className='text-center mt-3'>
               <Button className='px-5 me-2'>수정</Button>
               <Button className='px-5' variant='secondary'>취소</Button>
