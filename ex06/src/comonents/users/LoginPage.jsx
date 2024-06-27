3652f0992d5c9606981851064a2476217d27257d
helen-cho <hd9536@daum.net>
2024-06-27 17:52:11 +0900

    로그인

--- ex06/src/comonents/users/LoginPage.jsx
+++ ex06/src/comonents/users/LoginPage.jsx
@@ -1,17 +1,41 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 import {Row, Col, Card, Form, Button} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 const LoginPage = () => {
+  const [form, setForm] = useState({
+    uid:'red',
+    upass:'pass'
+  });
+  const {uid, upass} = form;
+  const onChangeForm = (e) => {
+    setForm({...form, [e.target.name]:e.target.value});
+  }
+
+  const onSumbit = async(e) => {
+    e.preventDefault();
+    const res=await axios.post('/users/login', form);
+    if(res.data===1){
+      alert('로그인성공!');
+    }else if(res.data===2){
+      alert('비밀번호 불일치!');
+    }else if(res.data===0) {
+      alert('아이디가 존재하지않습니다!');
+    }
+  }
+
   return (
     <Row className='justify-content-center my-5'>
       <Col xs={8} md={6} lg={4}>
         <Card>
           <Card.Header><h3 className='text-center'>로그인</h3></Card.Header>
           <Card.Body>
-            <form>
-              <Form.Control placeholder='아이디' className='mb-2'/>
-              <Form.Control placeholder='비밀번호' className='mb-2'/>
-              <Button className='w-100'>로그인</Button>
+            <form onSubmit={onSumbit}>
+              <Form.Control value={uid} name='uid' onChange={onChangeForm}
+                placeholder='아이디' className='mb-2'/>
+              <Form.Control value={upass} name='upass'onChange={onChangeForm}
+                type='password' placeholder='비밀번호' className='mb-2'/>
+              <Button className='w-100' type='submit'>로그인</Button>
             </form>
             <div className='mt-2 text-end'>
               <Link to='/users/join'>회원가입</Link>
